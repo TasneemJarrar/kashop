c426c144d6362cc3e3a115b0908c01e55fa1a5ff
Tasneem_jarrar <tasneem.a.jarrar@gmail.com>
2026-03-28 23:24:52 +0300

    fix issue

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>  
+      <Typography component={'h2'} variant='h1'>Home</Typography>
+      <Typography component={'p'} variant='body2'>This is the home page.</Typography>
+    </div>
   )
 }

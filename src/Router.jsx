5b02dc7a17f7a962b84bf6aaf3ff0aad789a04d6
TasneemJarrar <tasneem.a.jarrar@gmail.com>
2026-06-26 23:12:16 +0300

    fix errors

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,9 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";import Home from "./pages/Home/Home";
 import Products from "./pages/products/Products";
 import Cart from "./pages/cart/Cart";
 import Login from "./pages/login/Login";
 import Register from "./pages/Register/Register";
+import MainLayout from "./layouts/mainlyout/MainLayout";
 
 const router = createBrowserRouter([
   {

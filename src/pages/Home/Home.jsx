5468a728c38c8a7ca980c5e11554e5f5de6fd8b2
TasneemJarrar <tasneem.a.jarrar@gmail.com>
2026-06-28 21:10:59 +0300

    add register form

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Typography variant="h1" component="h2">
+        Home
+      </Typography>
+      <Button variant="outlined">Contained</Button>
+    </div>
   )
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,38 @@
+import { Box, Button, TextField, Typography } from '@mui/material'
+import axios from 'axios';
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
 export default function Register() {
+  const { register, handleSubmit } = useForm ();
+
+  const RegisterForm = async (data) => {
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_BURL}/auth/Account/register`, data); 
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error', error);
+    }
+  }
+
   return (
-    <div>Register</div>
+    <Box component="section" sx={{ mt: 5 }}>
+      <Typography variant="h1" component="h2">
+        Register
+      </Typography>
+
+      <Box onSubmit={handleSubmit(RegisterForm)} component="form" sx={{ display: 'flex', flexDirection: 'column', gap: 2, mt: 2 }}>
+      <TextField {...register('userName')} label="User Name" variant="standard" />
+      <TextField {...register('fullName')} label="Full Name" variant="standard" />
+      <TextField {...register('email')} label="Email" variant="standard" />
+      <TextField {...register('phoneNumber')} label="Phone Number" variant="standard" />
+      <TextField {...register('password')} label="Password" variant="standard" />
+      <Button variant="outlined" type="submit">
+        Register
+      </Button>
+      </Box>
+
+
+    </Box>
   )
 }

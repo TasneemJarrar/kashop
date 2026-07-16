3729fc223a2913a1065dc5dce585fc5cfab740f6
TasneemJarrar <tasneem.a.jarrar@gmail.com>
2026-07-16 21:43:49 +0300

    add Update Cart Item logic, add cart count badge

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import useAuthStore from '../../store/useAuthStore';
 import { Link, useNavigate } from 'react-router';
+import useCart from '../../hooks/useCart';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import Badge, { badgeClasses } from '@mui/material/Badge';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 export default function Navbar() {
+  const { data } = useCart();
   const navigate = useNavigate();
   const token = useAuthStore((state) => state.token);
   const logout = useAuthStore((state) => state.logout);
@@ -11,12 +17,25 @@ export default function Navbar() {
     navigate('/login');
   }
 
+  const cartCount = data?.items?.length || 0;
+  const CartBadge = styled(Badge)`
+  & .${badgeClasses.badge} {
+    top: -12px;
+    right: -2px;
+  }
+`;
+
   return (
     <nav>
       <Link to="/">Home</Link>
       {token ?
         <>
-          <Link to="/cart">Cart</Link>
+          <Link to="/cart">
+            <IconButton aria-label="view cart with 2 items">
+              <ShoppingCartIcon fontSize="small" />
+              <CartBadge badgeContent={cartCount} color="secondary" overlap="circular" />
+            </IconButton>
+          </Link>
           <Link to="/login" component="button" onClick={handleLogout}>
             Logout
           </Link>
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,16 +1,32 @@
 import useAuthStore from '../../store/useAuthStore';
 import useCart from '../../hooks/useCart';
-import { Box, Button, CircularProgress, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import { Box, Button, CircularProgress, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 import useDeleteFromCart from '../../hooks/useRemoveFromCart';
-
+import useUpdateCartItem from '../../hooks/useUpdateCartItem';
+import DeleteIcon from '@mui/icons-material/Delete';
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Cart() {
   const {mutate:RemoveItem, isPending} = useDeleteFromCart();
+  const {mutate:UpdateItem, isPending: updateItemPending} = useUpdateCartItem();
 
   const { data, isLoading, isError, error } = useCart();
   const token = useAuthStore((state) => state.token);
 
-  console.log('data', data);
+  const handleUpdate = (productId, action)=>{
+    const item = data.items.find(i=>i.productId==productId);
+    if(action == '+'){
+    UpdateItem({productId, count: item.count+1})
+    } else {
+      if (item.count === 1) {
+        RemoveItem(productId);
+        return;
+      }
+      UpdateItem({productId, count: item.count-1})
+    }
+  }
+
   if (isLoading) { return <CircularProgress /> }
   if (isError) { return <Box color='red'>Error: {error.message}</Box> }
 
@@ -32,18 +48,30 @@ export default function Cart() {
 
           <TableBody>
 
-            {data.items.map((item) => (
+            {data?.items?.map((item) => (
               <TableRow key={item.productId}>
                 <TableCell>{item.productName}</TableCell>
                 <TableCell>{item.price}$</TableCell>
-                <TableCell>{item.count}</TableCell>
+                <TableCell>
+                  <Box sx={{display:'flex', gap:'1', alignItems:'center'}}>
+                  <IconButton size='small'>
+                    <RemoveIcon fontSize="small" onClick ={()=>handleUpdate (item.productId, '-')}/>
+                  </IconButton>
+                  <Typography>
+                  {item.count}
+                  </Typography>
+                  <IconButton size='small'>
+                    <AddIcon fontSize="small" onClick ={()=>handleUpdate (item.productId, '+')}/>
+                  </IconButton>
+                  </Box>
+                  </TableCell>
                 <TableCell>{item.totalPrice}$</TableCell>
                 <TableCell>
-                  <Button variant="contained" color="error" 
+                  <IconButton color="error" 
                   disabled={isPending}
                   onClick={()=> RemoveItem(item.productId )} >
-                    Remove
-                  </Button>
+                    <DeleteIcon />
+                  </IconButton>
                 </TableCell>
               </TableRow>
             ))}
